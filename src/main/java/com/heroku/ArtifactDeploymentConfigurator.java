86978fc5b3cb7d123c551ef916e4d7cf8aa683d6
Ryan Brainard <brainard@heroku.com>
2012-03-27 21:20:19 -0700

    use task helper

--- src/main/java/com/heroku/ArtifactDeploymentConfigurator.java
+++ src/main/java/com/heroku/ArtifactDeploymentConfigurator.java
@@ -4,24 +4,25 @@ import com.atlassian.bamboo.collections.ActionParametersMap;
 import com.atlassian.bamboo.task.AbstractTaskConfigurator;
 import com.atlassian.bamboo.task.TaskDefinition;
 import com.atlassian.bamboo.utils.error.ErrorCollection;
+import com.google.common.collect.ImmutableList;
 import com.opensymphony.xwork.TextProvider;
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Map;
 
 public class ArtifactDeploymentConfigurator extends AbstractTaskConfigurator {
     private TextProvider textProvider;
 
+    private static final List<String> FIELDS_TO_COPY = ImmutableList.of("apiKey", "appName", "artifactPath");
+
     @NotNull
     @Override
     public Map<String, String> generateTaskConfigMap(@NotNull final ActionParametersMap params, @Nullable final TaskDefinition previousTaskDefinition) {
         final Map<String, String> config = super.generateTaskConfigMap(params, previousTaskDefinition);
-        config.put("apiKey", params.getString("apiKey"));
-        config.put("appName", params.getString("appName"));
-        config.put("artifactPath", params.getString("artifactPath"));
+        taskConfiguratorHelper.populateTaskConfigMapWithActionParameters(config, params, FIELDS_TO_COPY);
         return config;
     }
 
@@ -35,27 +36,18 @@ public class ArtifactDeploymentConfigurator extends AbstractTaskConfigurator {
     @Override
     public void populateContextForEdit(@NotNull final Map<String, Object> context, @NotNull final TaskDefinition taskDefinition) {
         super.populateContextForEdit(context, taskDefinition);
-
-        populateForViewAndEdit(context, taskDefinition);
+        taskConfiguratorHelper.populateContextWithConfiguration(context, taskDefinition, FIELDS_TO_COPY);
     }
 
     @Override
     public void populateContextForView(@NotNull final Map<String, Object> context, @NotNull final TaskDefinition taskDefinition) {
         super.populateContextForView(context, taskDefinition);
-
-        populateForViewAndEdit(context, taskDefinition);
-    }
-
-    private void populateForViewAndEdit(Map<String, Object> context, TaskDefinition taskDefinition) {
-        context.put("apiKey", taskDefinition.getConfiguration().get("apiKey"));
-        context.put("appName", taskDefinition.getConfiguration().get("appName"));
-        context.put("artifactPath", taskDefinition.getConfiguration().get("artifactPath"));
+        taskConfiguratorHelper.populateContextWithConfiguration(context, taskDefinition, FIELDS_TO_COPY);
     }
 
     @Override
     public void validate(@NotNull final ActionParametersMap params, @NotNull final ErrorCollection errorCollection) {
         super.validate(params, errorCollection);
-
         final String apiKey = params.getString("apiKey");
         if (StringUtils.isEmpty(apiKey)) {
             errorCollection.addError("apiKey", textProvider.getText("com.heroku.say.error"));
