d41b3e9e5f1a89889c83de02e308f371219f569b
Ryan Brainard <brainard@heroku.com>
2012-03-29 14:33:12 -0700

    use java facade for client and add test

--- src/main/java/com/heroku/ArtifactDeployment.java
+++ src/main/java/com/heroku/ArtifactDeployment.java
@@ -2,13 +2,29 @@ package com.heroku;
 
 import com.atlassian.bamboo.build.logger.BuildLogger;
 import com.atlassian.bamboo.task.*;
-import com.herokuapp.directto.client.WarDeployer;
+import com.herokuapp.directto.client.DirectToHerokuClientForJava;
 import org.jetbrains.annotations.NotNull;
-import scala.collection.immutable.Map;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ArtifactDeployment implements TaskType {
+
+    private final StaticSandbox staticSandbox;
+
+    public ArtifactDeployment() {
+        this(new StaticSandbox() {
+            public TaskResult success(TaskContext taskContext) {
+                return TaskResultBuilder.create(taskContext).success().build();
+            }
+        });
+    }
+
+    public ArtifactDeployment(StaticSandbox staticSandbox) {
+        this.staticSandbox = staticSandbox;
+    }
+
     @NotNull
     @java.lang.Override
     public TaskResult execute(@NotNull final TaskContext taskContext) throws TaskException {
@@ -18,10 +34,19 @@ public class ArtifactDeployment implements TaskType {
         final String appName = taskContext.getConfigurationMap().get("appName");
         final String artifactPath = taskContext.getConfigurationMap().get("artifactPath");
 
-        final WarDeployer warDeploy = new WarDeployer(apiKey);
-        final Map<String, String> deployResult = warDeploy.deploy(appName, new File(artifactPath));
-        buildLogger.addBuildLogEntry(deployResult.toString());
+        final DirectToHerokuClientForJava client = new DirectToHerokuClientForJava(apiKey);
 
-        return TaskResultBuilder.create(taskContext).success().build();
+        final Map<String, File> files = new HashMap<String, File>(1);
+        files.put("war", new File(artifactPath));
+        final Map<String, String> deployResults = client.deploy("war", appName, files);
+        for (Map.Entry<String, String> result : deployResults.entrySet()) {
+            buildLogger.addBuildLogEntry(result.getKey() + ":" + result.getValue());
+        }
+        
+        return staticSandbox.success(taskContext);
+    }
+
+    static interface StaticSandbox {
+        TaskResult success(TaskContext taskContext);
     }
 }
