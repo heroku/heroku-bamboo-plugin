5faecb580216b663c0abcf7ea27a7903a2bd27e0
Ryan Brainard <brainard@heroku.com>
2012-04-06 11:56:45 -0700

    remove dependency on local file for tests

--- src/test/java/com/heroku/ArtifactDeploymentTasksTest.java
+++ src/test/java/com/heroku/ArtifactDeploymentTasksTest.java
@@ -12,6 +12,9 @@ import org.jmock.core.InvocationMatcher;
 import org.jmock.core.matcher.AnyArgumentsMatcher;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ArtifactDeploymentTasksTest extends MockObjectTestCase {
 
@@ -21,6 +24,7 @@ public class ArtifactDeploymentTasksTest extends MockObjectTestCase {
     private final Mock mockSuccessfulTaskResult = new Mock(TaskResult.class);
     private final Mock mockFailedTaskResult = new Mock(TaskResult.class);
     private final ConfigurationMapImpl configMap = new ConfigurationMapImpl();
+    private final File workingDir = createTempDir();
 
     @Override
     protected void setUp() throws Exception {
@@ -30,7 +34,7 @@ public class ArtifactDeploymentTasksTest extends MockObjectTestCase {
         mockStatics.expects(anything()).method("failed").will(returnValue(mockFailedTaskResult.proxy()));
         mockContext.expects(once()).method("getBuildLogger").will(returnValue(mockLogger.proxy()));
         mockContext.expects(atLeastOnce()).method("getConfigurationMap").will(returnValue(configMap));
-        mockContext.expects(atLeastOnce()).method("getWorkingDirectory").will(returnValue(new File(System.getProperty("heroku.workingDir"))));
+        mockContext.expects(atLeastOnce()).method("getWorkingDirectory").will(returnValue(workingDir));
         mockLogger.expects(anything()).method("addBuildLogEntry");
         mockLogger.expects(anything()).method("addErrorLogEntry");
         mockSuccessfulTaskResult.expects(anything()).method("getTaskState").will(returnValue(TaskState.SUCCESS));
@@ -45,23 +49,48 @@ public class ArtifactDeploymentTasksTest extends MockObjectTestCase {
     }
     
     public void testWarDeployment() throws Exception {
-        configMap.put("war", System.getProperty("heroku.warFile"));
+        configMap.put("war", File.createTempFile("some", ".war", workingDir).getName());
         assertEquals(TaskState.SUCCESS, runTask(WarDeploymentTask.class, WarDeploymentTaskConfigurator.class).getTaskState());
     }
 
     public void testFatJarDeployment() throws Exception {
-        configMap.put("jar", System.getProperty("heroku.jarFile"));
-        configMap.put("procfile", System.getProperty("heroku.procfileFile"));
+        configMap.put("jar", File.createTempFile("some", ".jar", workingDir).getName());
+        configMap.put("procfile", createProcfile(workingDir).getName());
         assertEquals(TaskState.SUCCESS, runTask(FatJarDeploymentTask.class, FatJarDeploymentTaskConfigurator.class).getTaskState());
     }
 
     public void testTarGzDeployment() throws Exception {
-        configMap.put("targz", System.getProperty("heroku.targzFile"));
-        configMap.put("procfile", System.getProperty("heroku.procfileFile"));
+        configMap.put("targz", File.createTempFile("some", ".tar.gz", workingDir).getName());
+        configMap.put("procfile", createProcfile(workingDir).getName());
         assertEquals(TaskState.SUCCESS, runTask(TarGzDeploymentTask.class, TargGzDeploymentTaskConfigurator.class).getTaskState());
     }
 
     InvocationMatcher anything() {
         return new AnyArgumentsMatcher();
+    }
+    
+    private static File createTempDir() {
+        try {
+            final File tmp = File.createTempFile("temp", "dir");
+            if (!tmp.delete()) throw new IOException("Could not delete");
+            if (!tmp.mkdir()) throw new IOException("Could not mkdir");
+            return tmp;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private File createProcfile(File dir) {
+        try {
+            final File procfile = File.createTempFile("Procfile", "", dir);
+            PrintWriter writer = new PrintWriter(procfile);
+            writer.append("web: exit");
+            writer.close();
+            return procfile;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
