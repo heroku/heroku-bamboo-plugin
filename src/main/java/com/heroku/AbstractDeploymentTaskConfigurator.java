05d42e9368c07d354e672fa9e33ce1538ab04d0a
Ryan Brainard <brainard@heroku.com>
2012-07-24 17:28:10 -0700

    api key encryption testing

--- src/main/java/com/heroku/AbstractDeploymentTaskConfigurator.java
+++ src/main/java/com/heroku/AbstractDeploymentTaskConfigurator.java
@@ -5,36 +5,19 @@ import com.atlassian.bamboo.security.EncryptionException;
 import com.atlassian.bamboo.security.StringEncrypter;
 import com.atlassian.bamboo.task.AbstractTaskConfigurator;
 import com.atlassian.bamboo.task.TaskDefinition;
-import com.atlassian.bamboo.user.BambooAuthenticationContext;
 import com.atlassian.bamboo.utils.error.ErrorCollection;
-import com.atlassian.bamboo.utils.i18n.I18nBean;
-import com.atlassian.bamboo.ww2.actions.build.admin.config.task.ConfigureBuildTasks;
 import com.google.common.collect.ImmutableList;
-import com.opensymphony.xwork.TextProvider;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractDeploymentTaskConfigurator extends AbstractTaskConfigurator implements DeploymentPipeline {
 
-    static final String API_KEY = "apiKey";
-    private static final String APP_NAME = "appName";
-
-    protected List<String> getFieldsToCopy() {
-        return ImmutableList.<String>builder().add(API_KEY, APP_NAME).addAll(getRequiredFiles()).build();
-    }
-
-    @NotNull
-    @Override
-    public Map<String, String> generateTaskConfigMap(@NotNull final ActionParametersMap params, @Nullable final TaskDefinition previousTaskDefinition) {
-        final Map<String, String> config = super.generateTaskConfigMap(params, previousTaskDefinition);
-        taskConfiguratorHelper.populateTaskConfigMapWithActionParameters(config, params, getFieldsToCopy());
-        return config;
-    }
+    protected static final String API_KEY = "apiKey";
+    protected static final String APP_NAME = "appName";
 
     @Override
     public void populateContextForCreate(@NotNull final Map<String, Object> context) {
@@ -53,29 +36,42 @@ public abstract class AbstractDeploymentTaskConfigurator extends AbstractTaskCon
         taskConfiguratorHelper.populateContextWithConfiguration(context, taskDefinition, getFieldsToCopy());
     }
 
+    @NotNull
+    @Override
+    public Map<String, String> generateTaskConfigMap(@NotNull final ActionParametersMap params, @Nullable final TaskDefinition previousTaskDefinition) {
+        final Map<String, String> config = super.generateTaskConfigMap(params, previousTaskDefinition);
+        taskConfiguratorHelper.populateTaskConfigMapWithActionParameters(config, params, getFieldsToCopy());
+        return config;
+    }
+
+    protected List<String> getFieldsToCopy() {
+        return ImmutableList.<String>builder().add(API_KEY, APP_NAME).addAll(getRequiredFiles()).build();
+    }
+
     @Override
     public void validate(@NotNull final ActionParametersMap params, @NotNull final ErrorCollection errorCollection) {
         super.validate(params, errorCollection);
-        
+        enforceFieldRequiredness(params, errorCollection);
+        encryptApiKey(params);
+    }
+
+    protected void enforceFieldRequiredness(ActionParametersMap params, ErrorCollection errorCollection) {
         for (String field : getFieldsToCopy()) {
             if (StringUtils.isEmpty(params.getString(field))) {
                 errorCollection.addError(field, getI18nBean().getText("com.heroku.errors.emptyField"));
             }
         }
+    }
 
+    protected void encryptApiKey(ActionParametersMap params) {
         if (params.containsKey(API_KEY)) {
-            if (!(params.get(API_KEY) instanceof String[])) { throw new RuntimeException("Unexpected API_KEY format"); }
-            final String[] apiKeyArray = (String[]) params.get(API_KEY);
-            if (apiKeyArray.length != 1) { throw new RuntimeException("Unexpected API_KEY array length"); }
-            final String apiKey = apiKeyArray[0];
-
             final StringEncrypter stringEncrypter = new StringEncrypter();
             try {
                 // test if the key is already encrypted
-                stringEncrypter.decrypt(apiKey);
+                stringEncrypter.decrypt(params.getString(API_KEY));
             } catch (EncryptionException e) {
                 // otherwise, encrypt it
-                params.put(API_KEY, new String[]{stringEncrypter.encrypt(apiKey)});
+                params.put(API_KEY, stringEncrypter.encrypt(params.getString(API_KEY)));
             }
         }
     }
---
+++ src/test/java/com/heroku/ArtifactDeploymentTasksTest.java
@@ -0,0 +1,66 @@
+package com.heroku;
+
+import com.atlassian.bamboo.collections.ActionParametersMap;
+import com.atlassian.bamboo.security.StringEncrypter;
+import com.atlassian.bamboo.webwork.util.ActionParametersMapImpl;
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.heroku.AbstractDeploymentTaskConfigurator.API_KEY;
+
+/**
+ * @author Ryan Brainard
+ */
+public class ArtifactDeploymentTasksTest extends MockObjectTestCase {
+
+    private static final String PLAIN_TEXT_KEY = "PLAIN_TEXT";
+    private static final String ENCRYPTED_KEY = new StringEncrypter().encrypt(PLAIN_TEXT_KEY);
+
+    public void testApiKeyEncryption_Blank() throws Exception {
+        final Mock mockParams = new Mock(ActionParametersMap.class);
+        mockParams.expects(atLeastOnce()).method("containsKey").will(returnValue(false));
+        mockParams.expects(never()).method("getString");
+        mockParams.expects(never()).method("put");
+
+        new TestingDeploymentTaskConfigurator().encryptApiKey((ActionParametersMap) mockParams.proxy());
+        mockParams.verify();
+    }
+
+    public void testApiKeyEncryption_New() throws Exception {
+        final Mock mockParams = new Mock(ActionParametersMap.class);
+        mockParams.expects(atLeastOnce()).method("containsKey").will(returnValue(true));
+        mockParams.expects(atLeastOnce()).method("getString").will(returnValue(PLAIN_TEXT_KEY));
+        mockParams.expects(once()).method("put").with(eq(API_KEY), eq(ENCRYPTED_KEY));
+
+        new TestingDeploymentTaskConfigurator().encryptApiKey((ActionParametersMap) mockParams.proxy());
+        mockParams.verify();
+    }
+
+    public void testApiKeyEncryption_Existing() throws Exception {
+        final Mock mockParams = new Mock(ActionParametersMap.class);
+        mockParams.expects(atLeastOnce()).method("containsKey").will(returnValue(true));
+        mockParams.expects(atLeastOnce()).method("getString").will(returnValue(ENCRYPTED_KEY));
+        mockParams.expects(never()).method("put");
+
+        new TestingDeploymentTaskConfigurator().encryptApiKey((ActionParametersMap) mockParams.proxy());
+        mockParams.verify();
+    }
+
+    private static class TestingDeploymentTaskConfigurator extends AbstractDeploymentTaskConfigurator {
+
+        @Override
+        public String getPipelineName() {
+            return "TEST";
+        }
+
+        @Override
+        public List<String> getRequiredFiles() {
+            return Collections.emptyList();
+        }
+    }
+
+}
